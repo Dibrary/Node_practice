ce6e348763bbd031a4fe7f27cd5a98fa8a9f40d3
Dibrary <kkdh828@hanmail.net>
2022-02-24 11:49:44 +0900

    socket.io 코드 작성 및 테스트 확인

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-    res.render('index');
+    // res.render('index'); // 웹소켓 연습
+    res.render('index2'); // socket.io 연습
 });
 
 module.exports = router;
