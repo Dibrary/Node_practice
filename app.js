bab9bab2b5309b0ae7bb6c3f8e0c17e0a7c78e58
Dibrary <kkdh828@hanmail.net>
2022-02-24 11:24:34 +0900

    웹소켓 적용해서 test 확인

--- app.js
+++ app.js
@@ -1,32 +1,23 @@
-const express = require('express'); // 익스프레스 모듈 불러오기
-const cookieParser = require('cookie-parser'); // 쿠키관련 처리를 위해 
-const morgan = require('morgan'); // 로그 남겨주기
-const path = require('path'); // 경로관련
+
+const express = require('express');
+const path = require('path');
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
 const session = require('express-session');
-const nunjucks = require('nunjucks'); // html 템플릿
-const dotenv = require('dotenv'); // env파일 설정
-const {sequelize} = require('./models');
+const nunjucks = require('nunjucks');
+const dotenv= require('dotenv');
 
 dotenv.config();
-
-const pageRouter = require('./routes/page');
-const userRouter = require('./routes/user');
+const webSocket = require('./socket');
+const indexRouter = require('./routes');
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
+app.set('port', process.env.PORT || 8005);
 app.set('view engine', 'html');
 nunjucks.configure('views', {
     express:app,
     watch:true,
 });
-
-
-sequelize.sync({force:false}).then(() => {
-    console.log("데이터베이스 연결 성공");
-}).catch((err) => {
-    console.error(err);
-});
-
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -34,8 +25,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:false}));
 app.use(cookieParser(process.env.COOKIE_SECRET));
 app.use(session({
-    resave: false,
-    saveUninitialized: false,
+    resave:false,
+    saveUninitialized:false,
     secret: process.env.COOKIE_SECRET,
     cookie: {
         httpOnly: true,
@@ -43,34 +34,97 @@ app.use(session({
     },
 }));
 
-// app.use('/', pageRouter);
-app.use('/', pageRouter);
+app.use('/', indexRouter);
 
-app.get('/', (req, res, next) => {
-    console.log('GET 요청에서만 실행됩니다.');
+app.use((req, res, next) => {
+    const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+    error.status = 404;
+    next(error);
 });
 
-// app.use((req, res, next) => {
-//     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
-//     error.status = 404;
-//     next(error);
-// });
+app.use((err, req, res, next) => {
+    res.locals.message = err.message;
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err: {};
+    res.status(err.status ||500);
+    res.render('error');
+});
 
-// app.use((err, req, res, next) => {
-//     res.locals.message = err.message;
-//     res.locals.error = process.env.NODE_ENV !== 'production'? err:{};
-//     res.status(err.status || 500);
-//     res.render('error');
-// });
-
-app.listen(app.get('port'), () => {
+const server = app.listen(app.get('port'), () => {
     console.log(app.get('port'), '번 포트에서 대기 중');
 });
 
+webSocket(server);
 
 
 
 
+// 아래 코드는 Node 이용해서 MySQL 접속 연습
+
+// const express = require('express'); // 익스프레스 모듈 불러오기
+// const cookieParser = require('cookie-parser'); // 쿠키관련 처리를 위해 
+// const morgan = require('morgan'); // 로그 남겨주기
+// const path = require('path'); // 경로관련
+// const session = require('express-session');
+// const nunjucks = require('nunjucks'); // html 템플릿
+// const dotenv = require('dotenv'); // env파일 설정
+// const {sequelize} = require('./models');
+
+// dotenv.config();
+
+// const pageRouter = require('./routes/page');
+// const userRouter = require('./routes/user');
+
+// const app = express();
+// app.set('port', process.env.PORT || 3000);
+// app.set('view engine', 'html');
+// nunjucks.configure('views', {
+//     express:app,
+//     watch:true,
+// });
 
 
+// sequelize.sync({force:false}).then(() => {
+//     console.log("데이터베이스 연결 성공");
+// }).catch((err) => {
+//     console.error(err);
+// });
 
+
+// app.use(morgan('dev'));
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.json());
+// app.use(express.urlencoded({extended:false}));
+// app.use(cookieParser(process.env.COOKIE_SECRET));
+// app.use(session({
+//     resave: false,
+//     saveUninitialized: false,
+//     secret: process.env.COOKIE_SECRET,
+//     cookie: {
+//         httpOnly: true,
+//         secure: false,
+//     },
+// }));
+
+// // app.use('/', pageRouter);
+// app.use('/', pageRouter);
+
+// app.get('/', (req, res, next) => {
+//     console.log('GET 요청에서만 실행됩니다.');
+// });
+
+// // app.use((req, res, next) => {
+// //     const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+// //     error.status = 404;
+// //     next(error);
+// // });
+
+// // app.use((err, req, res, next) => {
+// //     res.locals.message = err.message;
+// //     res.locals.error = process.env.NODE_ENV !== 'production'? err:{};
+// //     res.status(err.status || 500);
+// //     res.render('error');
+// // });
+
+// app.listen(app.get('port'), () => {
+//     console.log(app.get('port'), '번 포트에서 대기 중');
+// });
